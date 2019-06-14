c164ebb645327a10c5a2e89a845773e29284a90b
Piotr Błażejewicz (Peter Blazejewicz) <peterblazejewicz@users.noreply.github.com>
2019-06-14 23:09:22 +0200

    Update 01-login to Angular 8 (#12)
    * Update setup to Angular 8
    
    This was done by issuing `ng update ...` command as per
    Angular 8 migration docs.
    
    Thanks!
    
    * Update non-Angular dependencies to latest versions
    
    * Minor fixes after update from master
    
    - rebase artefacts corrected

--- 01-Login/src/app/app.component.spec.ts
+++ 01-Login/src/app/app.component.spec.ts
@@ -21,23 +21,23 @@ describe('AppComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       imports: [
-        RouterTestingModule.withRoutes(routes),
-        NgbModule,
         FontAwesomeModule,
         HighlightModule.forRoot({
           languages: hljsLanguages
-        })
+        }),
+        NgbModule,
+        RouterTestingModule.withRoutes(routes)
       ],
       declarations: [
-        HomeComponent,
-        CallbackComponent,
         AppComponent,
+        CallbackComponent,
+        FooterComponent,
         HeroComponent,
+        HomeComponent,
         HomeContentComponent,
         LoadingComponent,
-        ProfileComponent,
         NavBarComponent,
-        FooterComponent
+        ProfileComponent
       ]
     }).compileComponents();
   }));
